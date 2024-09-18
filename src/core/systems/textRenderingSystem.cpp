b05b8ad9461dbdcf73dcf08512c479f4482ce773
Benjos <benjamin@bourge.be>
2024-09-18 19:05:03 +0100

    [~] Text rendering fixed when camera moving

--- src/core/systems/textRenderingSystem.cpp
+++ src/core/systems/textRenderingSystem.cpp
@@ -29,12 +29,15 @@ void TextRenderingSystem::update(float deltaTime)
         auto &text = coordinator->getComponent<Text>(entity);
         auto &transform = coordinator->getComponent<Transform>(entity);
 
+        Entity camera = coordinator->getEntityFromTag("camera");
+        auto &cameraTransform = coordinator->getComponent<Transform>(camera);
+
         sf::Text sfText;
         sfText.setFont(coordinator->_font);
         sfText.setString(text._text);
         sfText.setCharacterSize(text._fontSize);
         sfText.setFillColor(text._color);
-        sfText.setPosition(transform._x, transform._y);
+        sfText.setPosition(transform._x - cameraTransform._x + 1920 / 2, transform._y - cameraTransform._y + 1080 / 2);
         coordinator->_window->draw(sfText);
     }
 }
